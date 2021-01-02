dffc97b6dc4ef35617397e26afe091df5cf4b188
root <root@hecoswap.info>
2021-01-02 11:14:45 +0000

    change contract address

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,8 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
+    //uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
+    uri: 'http://hecoswap.info:8000/subgraphs/name/davekaj/uniswap',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
@@ -20,7 +21,8 @@ export const healthClient = new ApolloClient({
 
 export const v1Client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap',
+    //uri: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap',
+    uri: 'http://hecoswap.info:8000/subgraphs/name/davekaj/uniswap',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
